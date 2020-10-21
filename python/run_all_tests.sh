85dc00ab4f4159c941a0928834ae50ed65174041
Ehallhoff@gmail.com <ehallhoff@gmail.com>
2020-10-21 18:37:52 +0100

    Update script

--- python/run_all_tests.sh
+++ python/run_all_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# python3 state_space_estimation.py 'rbc' -s 1
-# python3 state_space_estimation.py 'rbc' -n 100 -c 1000 -s 1
-# python3 state_space_estimation.py 'nk' -s 1
+python3 state_space_estimation.py 'rbc' -s 1
+python3 state_space_estimation.py 'rbc' -n 100 -c 1000 -s 1
+python3 state_space_estimation.py 'nk' -s 1
 python3 state_space_estimation.py 'nk' -M 4 -n 100 -c 1000 -s 1
-python3 state_space_estimation.py 'real' -t 'custom_3' -s 1
-python3 state_space_estimation.py 'real' -t 'custom_4' -s 1
-# python3 state_space_estimation.py 'real' -t 'srivastava' -s 1
-# python3 state_space_estimation.py 'real' -t 'schott' -s 1
+python3 state_space_estimation.py 'real' -t -s 1
